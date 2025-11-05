2df3f248dc5e3dec9a204a4f91edf038284003d3
Spencer Schien <spencer.schien@gmail.com>
2025-11-05 14:28:50 -0600

    updated full_table

--- R/combine_tables.R
+++ R/combine_tables.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(wisconsink12)
+library(glue)
 
 public <- read_rds("data/public_schools_table.rda")
 private <- read_rds("data/private_schools_table.rda")
@@ -22,6 +24,25 @@ full_table <- private |>
         district_key = district_agency_key,
         download_url
       )
-  )
+  ) |>
+  mutate(school_year = glue("{year-1}-{year-2000}")) |>
+  select(school_year, everything()) |>
+  select(-year)
 
-saveRDS(full_table, "data/full_table.rda")
+
+with_codes <- left_join(
+  full_table,
+  schools |>
+    transmute(
+      school_year,
+      dpi_true_id,
+      school_key = str_pad(agency_key, width = 6, pad = "0", side = "left"),
+      accurate_agency_type,
+      broad_agency_type
+    )
+)
+
+saveRDS(with_codes, "data/full_table.rda")
+
+with_codes |>
+  filter(is.na(dpi_true_id))
